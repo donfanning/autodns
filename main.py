ead776aa24836787ecea255c6510290db07bc4f2
Amim Knabben <amim.knabben@gmail.com>
2016-05-08 21:07:42 -0300

    Using public ip address

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ import requests
 import argparse
 
 
-def create_dns_register(public_ip, private_ip, name, args):
+def create_dns_register(public_ip, name, args):
     """Create a DNS register on Consul
 
     Parameters
     ----------
     public_ip: string
         Public ip address from Instance
-    private_ip: string
-        Private ip addresss from Instance
     name: string
         Name of the instance (from tag)
     args: string
@@ -21,17 +19,16 @@ def create_dns_register(public_ip, private_ip, name, args):
     """
     data = {
         "Datacenter": "dc1",
-        "Address": private_ip,
+        "Address": public_ip,
         "Node": name,
-        "TaggedAddresses": {
-            "wan": public_ip
-        }
     }
     requests.put("http://{}/v1/catalog/register".format(args.consul),
                  json=data)
 
 
 def main():
+    """ Apply the DNS creation on all instance of EC2 """
+
     parser = argparse.ArgumentParser("")
     parser.add_argument('--consul', help="Consul address", default="localhost:8500")
     args = parser.parse_args()
@@ -40,12 +37,10 @@ def main():
         try:
             inst_obj = instance.get("Instances")[0]
             public_ip = inst_obj.get('PublicIpAddress')
-            private_ip = inst_obj.get('PrivateIpAddress')
             name_tags = filter(lambda item: item.get('Key') == 'Name',
                                inst_obj.get('Tags'))
             name = ''.join(map(lambda item: item.get('Value'), name_tags))
-
-            create_dns_register(public_ip, private_ip, name, args)
+            create_dns_register(public_ip, name, args)
         except IndexError:
             continue
 
