7b9d079810633bbdf2b530cc272e1ca65b307ec9
Amim Knabben <amim.knabben@gmail.com>
2016-08-19 17:04:21 -0300

    Adding reading capabilities

--- main.py
+++ main.py
@@ -1,29 +1,23 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
 import boto3
 import requests
 import argparse
 
 
-def create_dns_register(public_ip, name, args):
-    """Create a DNS register on Consul
+def get_dns_host(instances):
+    """ Get DNS list with PublicIpAddress """
+    from tabulate import tabulate
+    keys =instances.keys()
+    values =instances.values()
+    print(tabulate({"IP": values, "Name": keys}))
 
-    Parameters
-    ----------
-    public_ip: string
-        Public ip address from Instance
-    name: string
-        Name of the instance (from tag)
-    args: string
-        Arguments from script
-    """
-    data = {
-        "Datacenter": "dc1",
-        "Address": public_ip,
-        "Node": name,
-    }
-    requests.put("http://{}/v1/catalog/register".format(args.consul),
-                 json=data)
+
+def create_dns_register(consul, instances):
+    """Create a DNS register on Consul"""
+    for key, value in instances.iteritems():
+        data = {"Datacenter": "dc1", "Address": value, "Node": key}
+        requests.put("http://{}/v1/catalog/register".format(consul), json=data)
 
 
 def main():
@@ -31,18 +25,24 @@ def main():
 
     parser = argparse.ArgumentParser("")
     parser.add_argument('--consul', help="Consul address", default="localhost:8500")
-    args = parser.parse_args()
-    ec2 = boto3.client('ec2')
+    parser.add_argument('--read', action='store_true')
+    args, ec2 = parser.parse_args(), boto3.client('ec2')
+
+    instances = {}
     for instance in ec2.describe_instances()["Reservations"]:
         try:
             inst_obj = instance.get("Instances")[0]
             public_ip = inst_obj.get('PublicIpAddress')
-            name_tags = filter(lambda item: item.get('Key') == 'Name',
-                               inst_obj.get('Tags'))
+            name_tags = filter(lambda item: item.get('Key') == 'Name', inst_obj.get('Tags'))
             name = ''.join(map(lambda item: item.get('Value'), name_tags))
-            create_dns_register(public_ip, name, args)
+            instances[name] = public_ip
         except IndexError:
             continue
+
+    if args.read:
+        get_dns_host(instances)
+    else:
+        create_dns_register(args.consul, instances)
 
 if __name__ == "__main__":
     main()
