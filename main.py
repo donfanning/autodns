2e08b5c5a4c0549e83d711c96e7459cd9a40ea2f
Amim Knabben <amim.knabben@gmail.com>
2017-01-20 18:01:15 -0200

    Removing sg -> aws-tools

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import argparse
 
 from dns import DNS
 from queue import Queue
-from sg import SecurityGroup, AlreadyExists
 from es import ES
 
 
@@ -18,25 +17,12 @@ def main():
     parser.add_argument('--sqs', action='store_true', default=False)
     parser.add_argument('queues', nargs='*', default=[])
 
-    parser.add_argument('--sg_machine', default="")
-    parser.add_argument('--sg_enable_ip', default="")
-
     parser.add_argument('--es_ip', default="")
     parser.add_argument('--es_domain', default="")
 
     args = parser.parse_args()
 
-    if args.sg_machine and args.sg_enable_ip:
-        # Enable machine IP on security group for ssh
-        sg = SecurityGroup(args.sg_machine, args.sg_enable_ip)
-        try:
-            print(sg.add_rule())
-            print("Adicionando regra de desbloqueio")
-        except AlreadyExists:
-            print(sg.remove_rule())
-            print("Removendo regra de desbloqueio")
-
-    elif args.sqs:
+    if args.sqs:
         # If --sqs is passed list queues data
         queue = Queue(args.queues)
         queue.print_data(queue.get_sqs_list())
